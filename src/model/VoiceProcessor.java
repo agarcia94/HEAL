515bf97a874bd1ea113cba37493d885a736b1203
Andrew Garcia <mattgarcia94@sbcglobal.net>
2016-03-01 18:05:26 -0800

    voice processor

--- src/model/VoiceProcessor.java
+++ src/model/VoiceProcessor.java
@@ -9,18 +9,16 @@ import java.io.IOException;
 public class VoiceProcessor {
 	
 	private String voiceResult;
+	private VoiceCapture capture;
+	private StreamSpeechRecognizer recognizer;
 	
-	public String getVoiceResult() {
-		return voiceResult;
-	}
-
-	public void setVoiceResult(String voiceResult) {
-		this.voiceResult = voiceResult;
-	}
-
 	public VoiceProcessor(){
 		voiceResult = "";
-		VoiceCapture capture =null;
+		capture =null;
+		recognizer = null;
+	}
+	
+	public void captureAudio(){
 //		Player player = null;
 		Scanner sc = new Scanner(System.in);
 		int choice;
@@ -34,19 +32,13 @@ public class VoiceProcessor {
         configuration
                 .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-        StreamSpeechRecognizer recognizer;
+        
 		try {
 			recognizer = new StreamSpeechRecognizer(configuration);
-			
-			System.out.println("Please enter your choice: \n1: capture\n2: play");
-			
-			do{
-				choice = sc.nextInt();
-				if(choice ==1){
-					capture = new VoiceCapture();
-					capture.start();
-				}
-				else if(choice ==2){
+			capture = new VoiceCapture();
+			capture.start();
+			//System.out.println("Please enter your choice: \n1: capture\n2: play");
+				
 					capture.stopped = true;
 					if(!capture.readyToPlay){
 						System.out.println("Processing voice...");
@@ -64,15 +56,28 @@ public class VoiceProcessor {
 					
 					recognizer.stopRecognition();
 
-			}	
+				
 
-			}while(choice !=0);
-			//sc.close();
-		} catch (IOException e) {
+			
+		}catch (IOException e){
 			e.printStackTrace();
 		}
-
+	}
+	
+	public void stopCapture(){
 		
+	}
+	
+	public String getVoiceResult() {
+		return voiceResult;
+	}
+
+	public void setVoiceResult(String voiceResult) {
+		this.voiceResult = voiceResult;
+	}
+	
+	public VoiceCapture getCapture(){
+		return capture;
 	}
 
 	 
