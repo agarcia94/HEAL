1f2f7867ca89ecbc6ff8b58c261e7196e70bc050
andrewSRahn <andrewSRahn@gmail.com>
2016-03-06 16:26:12 -0800

    Revert "Revert "updated log in form""
    This reverts commit f365b4ddaecd0b6263665a80927c4710a9126d69.

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,47 +1,98 @@
 package controller;
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LoginController
- */
+
 @WebServlet("/LoginController")
+
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("	<meta charset=\"UTF-8\">");
+		out.println("	<title>Sessions Login</title>");
+		out.println("	<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<div class=\"container\">");
+		
+		out.println("	<div class=\"page-header\">");
+		out.println("		<h1>Login <small>Heal</small></h1>");
+		out.println("	</div>");
+		
+		String error = (String) request.getAttribute("error");
+		if (error != null)
+			out.println("<p class=\"text-danger lead\">" + error + "</p>");
+		
+		// Page-Specific Content
+		out.println("<form action=\"Login\" method=\"post\" >");
+		out.println("   Username: <input type=\"text\" name=\"username\" /> <br />");
+		out.println("   Password: <input type=\"password\" name=\"password\" /> <br />");
+		out.println("   <input type=\"submit\" value=\"Login\" />");
+		
+		out.println("</form>");
+		
+		out.println("</div>");
+		out.println("</body>");
+		out.println("</html>	");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		
+	
+		ArrayList<HealUsers> healUsers = (ArrayList<HealUsers>) getServletContext().getAttribute("HealUsers");
+		
+		for ( HealUsers user: healUsers){
+			PrintWriter out = response.getWriter();
+			
+			if (username != null && username.equalsIgnoreCase(user.getEmail())
+					&& password != null && password.equalsIgnoreCase(user.getPassword()) ){
+			
+				// Create a Session Variable named Username
+				HttpSession session = request.getSession();
+				session.setAttribute("CurrentUser", user);
+				
+				out.print("good:"+user);
+			    //Redirect to the Member's Only page
+				response.sendRedirect(".MyHeal");
+				//response.sendRedirect("../Sessions/Members");
+				
+				return;
+			 }
+		out.println("");
+			
+		}
+		request.setAttribute("error", "Invalid Username and/or Password");
+		doGet(request, response);
+	
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
